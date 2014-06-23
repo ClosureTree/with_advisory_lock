68fade92e71001e227a22ea4cedfd14dc7bbd59e
Matthew McEachen <matthew+github@mceachen.org>
2014-06-22 21:01:54 -0700

    bump travis

--- tests.sh
+++ tests.sh
@@ -2,7 +2,7 @@
 export BUNDLE_GEMFILE DB
 
 for BUNDLE_GEMFILE in gemfiles/*.gemfile ; do
-  for DB in sqlite mysql postgresql
+  for DB in mysql postgresql sqlite
   do
     echo $DB $BUNDLE_GEMFILE `ruby -v`
     bundle exec rake
