997e8071e26e7c7e1c2ec8b1f2485c32e9a49865
Matthew McEachen <matthew+github@mceachen.org>
2014-07-08 20:54:26 -0700

    call appraisal bundle update to fix missing gems [skip ci]

--- tests.sh
+++ tests.sh
@@ -5,6 +5,7 @@ for RUBY in 2.1.2 jruby-1.7.12 ; do
   rbenv local $RUBY
   for DB in mysql postgresql sqlite ; do
     echo "$DB | $(ruby -v)"
+    appraisal bundle update
     appraisal rake test
   done
 done
