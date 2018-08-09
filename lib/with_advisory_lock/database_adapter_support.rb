f8c105b1f3d2268b36f22668728f72b5dc657726
Ash Tyndall <git@atyndall.net>
2018-08-09 08:04:25 +0800

    Add caching by MySQL version

--- lib/with_advisory_lock/database_adapter_support.rb
+++ lib/with_advisory_lock/database_adapter_support.rb
@@ -1,5 +1,9 @@
 module WithAdvisoryLock
   class DatabaseAdapterSupport
+    # Caches nested lock support by MySQL reported version
+    @@mysql_nl_cache       = {}
+    @@mysql_nl_cache_mutex = Mutex.new
+
     def initialize(connection)
       @connection = connection
       @sym_name   = connection.adapter_name.downcase.to_sym
@@ -15,28 +19,36 @@ module WithAdvisoryLock
     def mysql_nested_lock_support?
       return false unless mysql?
 
-      lock_1 = "\"nested-test-1-#{SecureRandom.hex}\""
-      lock_2 = "\"nested-test-2-#{SecureRandom.hex}\""
+      # We select the MySQL version this way and cache on it, as MySQL will report versions like "5.7.5", and MariaDB will
+      # report versions like "10.3.8-MariaDB", which allow us to cache on features without introducing problems.
+      version = @connection.select_value("SELECT version()")
 
-      get_1  = @connection.select_value("SELECT GET_LOCK(#{lock_1}, 0) AS t#{SecureRandom.hex}")
-      get_2  = @connection.select_value("SELECT GET_LOCK(#{lock_2}, 0) AS t#{SecureRandom.hex}")
+      @@mysql_nl_cache_mutex.synchronize do
+        return @@mysql_nl_cache[version] if @@mysql_nl_cache.keys.include?(version)
 
-      # Both locks should succeed in old and new MySQL versions with "1"
-      raise RuntimeError, "Unexpected nested lock acquire result #{get_1}, #{get_2}" unless [get_1, get_2] == [1, 1]
+        lock_1 = "\"nested-test-1-#{SecureRandom.hex}\""
+        lock_2 = "\"nested-test-2-#{SecureRandom.hex}\""
 
-      release_1 = @connection.select_value("SELECT RELEASE_LOCK(#{lock_1}) AS t#{SecureRandom.hex}")
-      release_2 = @connection.select_value("SELECT RELEASE_LOCK(#{lock_2}) AS t#{SecureRandom.hex}")
+        get_1  = @connection.select_value("SELECT GET_LOCK(#{lock_1}, 0) AS t#{SecureRandom.hex}")
+        get_2  = @connection.select_value("SELECT GET_LOCK(#{lock_2}, 0) AS t#{SecureRandom.hex}")
 
-      # In MySQL <  5.7.5 release_1 will return  nil (not currently locked) and release_2 will return 1 (successfully unlocked)
-      # In MySQL >= 5.7.5 release_1 and release_2 will return 1 (both successfully unlocked)
-      # See https://dev.mysql.com/doc/refman/5.7/en/miscellaneous-functions.html#function_get-lock for more
-      case [release_1, release_2]
-      when [1, 1]
-        true
-      when [nil, 1]
-        false
-      else
-        raise RuntimeError, "Unexpected nested lock release result #{release_1}, #{release_2}"
+        # Both locks should succeed in old and new MySQL versions with "1"
+        raise RuntimeError, "Unexpected nested lock acquire result #{get_1}, #{get_2}" unless [get_1, get_2] == [1, 1]
+
+        release_1 = @connection.select_value("SELECT RELEASE_LOCK(#{lock_1}) AS t#{SecureRandom.hex}")
+        release_2 = @connection.select_value("SELECT RELEASE_LOCK(#{lock_2}) AS t#{SecureRandom.hex}")
+
+        # In MySQL <  5.7.5 release_1 will return  nil (not currently locked) and release_2 will return 1 (successfully unlocked)
+        # In MySQL >= 5.7.5 release_1 and release_2 will return 1 (both successfully unlocked)
+        # See https://dev.mysql.com/doc/refman/5.7/en/miscellaneous-functions.html#function_get-lock for more
+        @@mysql_nl_cache[version] = case [release_1, release_2]
+                                    when [1, 1]
+                                      true
+                                    when [nil, 1]
+                                      false
+                                    else
+                                      raise RuntimeError, "Unexpected nested lock release result #{release_1}, #{release_2}"
+                                    end
       end
     end
 
