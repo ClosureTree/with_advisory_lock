9e756815f5fb915801ceea1b6c12c3881acf6f5c
Abdelkader Boudih <terminale@gmail.com>
2025-05-27 15:48:47 +0100

    remove debug

--- lib/with_advisory_lock.rb
+++ lib/with_advisory_lock.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require 'with_advisory_lock/version'
-
 require 'active_support'
+require 'active_support/concern'
 
 module WithAdvisoryLock
   autoload :Concern, 'with_advisory_lock/concern'
@@ -20,7 +20,6 @@ end
 ActiveSupport.on_load :active_record do
   require 'active_record/connection_adapters/abstract_adapter'
   ActiveRecord::Base.include WithAdvisoryLock::Concern
-  puts '[WithAdvisoryLock] Loaded into ActiveRecord' if ENV['DEBUG_LOAD']
 end
 
 # JRuby compatibility handling
@@ -31,19 +30,16 @@ if RUBY_ENGINE == 'jruby'
 else
   # Standard adapter injection for MRI and TruffleRuby
   ActiveSupport.on_load :active_record_postgresqladapter do
-    puts '[WithAdvisoryLock] Loading into :active_record_postgresqladapter hook' if ENV['DEBUG_LOAD']
     prepend WithAdvisoryLock::CoreAdvisory
     prepend WithAdvisoryLock::PostgreSQLAdvisory
   end
 
   ActiveSupport.on_load :active_record_mysql2adapter do
-    puts '[WithAdvisoryLock] Loading into :active_record_mysql2adapter hook' if ENV['DEBUG_LOAD']
     prepend WithAdvisoryLock::CoreAdvisory
     prepend WithAdvisoryLock::MySQLAdvisory
   end
 
   ActiveSupport.on_load :active_record_trilogyadapter do
-    puts '[WithAdvisoryLock] Loading into :active_record_trilogyadapter hook' if ENV['DEBUG_LOAD']
     prepend WithAdvisoryLock::CoreAdvisory
     prepend WithAdvisoryLock::MySQLAdvisory
   end
